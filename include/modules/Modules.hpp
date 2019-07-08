dcd1eb1f6a0a0fd54904b526cf4ed92880730017
Bartek Banach <thebartekbanach.dev@gmail.com>
2019-07-08 16:42:44 +0200

    Add MainPumpModule to modules and remove TestModule

--- include/modules/Modules.hpp
+++ include/modules/Modules.hpp
@@ -2,18 +2,29 @@
 
 #include "../system/SystemModulesList.hpp"
 
-#include "TestModule.hpp"
+#include "../control/relayModule/RelayModule.hpp"
+
 #include "timeSetup/TimeSetupModule.hpp"
+#include "mainPump/Module.hpp"
 
 SystemModulesList* getSystemModules() {
-    TestModule* testModule = new TestModule();
-    TimeSetupModule* timeSetup = new TimeSetupModule();
+    logln("Initializing system dependencies");
+
+    RelayModule* relayModule = new RelayModule(8,
+        new ushort[8] { 52, 50, 48, 46, 44, 42, 40, 38 },
+        new bool[8] { HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH }
+    );
+
+    logln("Initializing system modules")
+    
+    TimeSetupModule* timeSetupModule = new TimeSetupModule();
+    MainPumpModule* mainPumpModule = new MainPumpModule(relayModule);
     
     #define NUMBER_OF_MODULES 2
 
     SystemModule** modules = new SystemModule*[NUMBER_OF_MODULES] {
-        testModule,
-        timeSetup
+        mainPumpModule,
+        timeSetupModule
     };
 
     return new SystemModulesList(modules, NUMBER_OF_MODULES);
