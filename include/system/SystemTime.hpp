08135539114829903bce0c92d6fb4211e0eb6ea8
Bartek Banach <thebartekbanach.dev@gmail.com>
2019-07-11 11:14:53 +0200

    Add operators to SystemTime

--- include/system/SystemTime.hpp
+++ include/system/SystemTime.hpp
@@ -5,6 +5,30 @@
 struct SystemTime {
     unsigned short hour;
     unsigned short minute;
+
+    friend bool operator == (const SystemTime& left, const SystemTime& right) {
+        return left.hour == right.hour && left.minute == right.minute;
+    }
+
+    friend bool operator < (const SystemTime& left, const RtcDateTime& right) {
+        if (right.Hour() != left.hour) return right.Hour() > left.hour;
+        else return right.Minute() > left.minute;
+    }
+
+    friend bool operator > (const SystemTime& left, const RtcDateTime& right) {
+        if (right.Hour() != left.hour) return right.Hour() < left.hour;
+        else return right.Minute() < left.minute;
+    }
+
+    friend bool operator < (const SystemTime& left, const SystemTime& right) {
+        if (right.hour != left.hour) return right.hour > left.hour;
+        else return right.minute > left.minute;
+    }
+
+    friend bool operator > (const SystemTime& left, const SystemTime& right) {
+        if (right.hour != left.hour) return right.hour < left.hour;
+        else return right.minute < left.minute;
+    }
 };
 
 SystemTime asSystemTime(const RtcDateTime& time) {
