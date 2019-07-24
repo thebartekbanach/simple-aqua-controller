5c10487978165f96f5341080966e0e3b8a289b03
Bartek Banach <thebartekbanach.dev@gmail.com>
2019-07-24 16:06:17 +0200

    Add WaterLevelSensor devices definition

--- include/control/waterLevelSensor/devices.hpp
+++ include/control/waterLevelSensor/devices.hpp
@@ -4,6 +4,12 @@
 #define changeWaterLevel 0
 #define normalWaterLevel 1
 
+#define changeWaterLevelPin (ushort) 31
+#define normalWaterLevelPin (ushort) 33
+
 #define addionalWaterTank 1
 #define addionalWaterTankMaxLevel 0
 #define addionalWaterTankMinLevel 1
+
+#define addionalWaterTankMaxLevelPin (ushort) 32
+#define addionalWaterTankMinLevelPin (ushort) 34
