14a95a530eb40b97cc834b545523168b3ce0baf0
Bartek Banach <thebartekbanach.dev@gmail.com>
2019-08-02 17:55:47 +0200

    Update water level sensor devices pins

--- include/control/waterLevelSensor/devices.hpp
+++ include/control/waterLevelSensor/devices.hpp
@@ -4,12 +4,12 @@
 #define changeWaterLevel 0
 #define normalWaterLevel 1
 
-#define changeWaterLevelPin (ushort) 31
-#define normalWaterLevelPin (ushort) 33
+#define changeWaterLevelPin (ushort) 30
+#define normalWaterLevelPin (ushort) 31
 
 #define addionalWaterTank 1
 #define addionalWaterTankMaxLevel 0
 #define addionalWaterTankMinLevel 1
 
 #define addionalWaterTankMaxLevelPin (ushort) 32
-#define addionalWaterTankMinLevelPin (ushort) 34
+#define addionalWaterTankMinLevelPin (ushort) 33
