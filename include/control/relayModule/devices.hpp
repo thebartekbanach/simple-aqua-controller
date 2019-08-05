b1d5d6b8da1760472d8401929ed8bdae39f4c80b
Bartek Banach <thebartekbanach.dev@gmail.com>
2019-08-05 14:14:23 +0200

    Update relay module pinout

--- include/control/relayModule/devices.hpp
+++ include/control/relayModule/devices.hpp
@@ -1,28 +1,28 @@
 #pragma once
 
 #define mainPump 0
-#define mainPumpPin 52
+#define mainPumpPin 38
 
 #define addionalPump 1
-#define addionalPumpPin 50
+#define addionalPumpPin 40
 
 #define heather 2
-#define heatherPin 48
+#define heatherPin 42
 
 #define aeration 3
-#define aerationPin 46
+#define aerationPin 44
 
 #define sterilization 4
-#define sterilizationPin 44
+#define sterilizationPin 46
 
 #define lighting 5
-#define lightingPin 42
+#define lightingPin 48
 
 #define addionalTankValve 6
-#define addionalTankValvePin 40
+#define addionalTankValvePin 50
 
 #define aquariumValve 7
-#define aquariumValvePin 38
+#define aquariumValvePin 50
 
 #define ON true
 #define OFF false
