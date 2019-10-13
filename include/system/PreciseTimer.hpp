f82da52e03897d50b5b6a50770a05f011000ed2f
Bartek Banach <thebartekbanach.dev@gmail.com>
2019-10-13 13:00:28 +0200

    Add safe precise timer

---
+++ include/system/PreciseTimer.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <Arduino.h>
+#include <limits.h>
+
+class PreciseTimer {
+    private:
+        unsigned long fireTime = 0; // need for rollover handle
+        unsigned long endTime = 0;
+
+    public:
+        PreciseTimer() {}
+        PreciseTimer(unsigned long howLongMs) {
+            start(howLongMs);
+        }
+
+        void start(unsigned long howLongMs) {
+            fireTime = millis();
+            endTime = fireTime + howLongMs;
+        }
+
+        unsigned long timeLeft() {
+            auto actualTime = millis();
+            
+            if (actualTime < fireTime) {
+                fireTime = 0;
+                endTime = endTime - ULONG_MAX;
+            }
+
+            if (actualTime > endTime) return 0;
+            else return endTime - actualTime;
+        }
+
+        bool done() {
+            return timeLeft() == 0;
+        }
+
+        operator bool() {
+            return done();
+        }
+};
