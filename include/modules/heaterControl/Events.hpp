358265127abe87d307e41cb79daa76e9e37fd70e
Bartek Banach <thebartekbanach.dev@gmail.com>
2019-10-23 13:25:01 +0200

    Add heater overheating safety lock

---
+++ include/modules/heaterControl/Events.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#define HEATER_MODULE_ID 4
+
+#define HEATER_SAFETY_LOCK 0
+#define HEATER_SAFETY_UNLOCK 1
--- include/modules/heaterControl/Module.hpp
+++ include/modules/heaterControl/Module.hpp
@@ -5,9 +5,34 @@
 
 #include "../../utils/TimeScopedDeviceDriver/Module.hpp"
 
+#include "Events.hpp"
+
 class HeaterControlModule: public TimeScopedDeviceDriver {
+    private:
+        bool heaterSafetyLock = false;
+
     public:
         HeaterControlModule(RelayModule* relayModule):
             TimeScopedDeviceDriver("Ogrzewanie", -1, relayModule, heater,
                 TimeScopedDeviceDriverSettings(CONTINUOUS_ON)) {}
+
+        bool isDeviceEnabled() override {
+            return !heaterSafetyLock;
+        }
+
+        void onEvent(const int &moduleId, const int &eventCode, void* data = nullptr) override {
+            if (moduleId == HEATER_MODULE_ID) {
+                switch (eventCode) {
+                    case HEATER_SAFETY_LOCK:
+                        heaterSafetyLock = true;
+                        break;
+                    case HEATER_SAFETY_UNLOCK:
+                        heaterSafetyLock = false;
+                        break;
+                    default: break;
+                }
+            }
+            
+            TimeScopedDeviceDriver::onEvent(moduleId, eventCode, data);
+        }
 };
