fcef018caaabfe89e3e979fb9d55c793e2543378
Bartek Banach <thebartekbanach.dev@gmail.com>
2019-11-02 12:12:15 +0100

    Change valve open/close timeout, add Adafruit PWM Servo Driver module configuration

--- include/control/valves/devices.hpp
+++ include/control/valves/devices.hpp
@@ -7,7 +7,7 @@
 #define NUMBER_OF_VALVES (unsigned short) 4
 #define SERVOS_OPEN_ANGLE (unsigned short) 0
 #define SERVOS_CLOSE_ANGLE (unsigned short) 180
-#define SERVOS_OPEN_CLOSE_TIMEOUT (unsigned short) 1500
+#define SERVOS_OPEN_CLOSE_TIMEOUT (unsigned short) 3000
 
 /*
     Servo valves name
@@ -46,3 +46,11 @@
 #define ADDIONAL_WATER_TANK_VALVE_CLOSE_DETECTION_PIN (unsigned short) 35
 #define CLEAN_WATER_VALVE_CLOSE_DETECTION_PIN (unsigned short) 37
 #define SEWAGE_WATER_VALVE_CLOSE_DETECTION_PIN (unsigned short) 39
+
+/*
+    Configuration for Adafruit PWM Servo Driver module
+*/
+
+#define ADAFRUIT_SERVO_DRIVER_SERVO_MIN 150 // This is the 'minimum' pulse length count (out of 4096)
+#define ADAFRUIT_SERVO_DRIVER_SERVO_MAX 600 // This is the 'maximum' pulse length count (out of 4096)
+#define ADAFRUIT_SERVO_DRIVER_FREQ 60 // Analog servos run at ~60 Hz updates
