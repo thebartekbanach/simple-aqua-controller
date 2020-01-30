9c2c150eef39475a4731fa91a43928ab32086be8
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-01-30 22:33:03 +0100

    Add more operators and helper functions, fix some bugs

--- include/system/SystemTime.hpp
+++ include/system/SystemTime.hpp
@@ -2,9 +2,15 @@
 
 #include <RtcDateTime.h>
 
+#define ONE_MINUTE 60
+#define ONE_HOUR (60 * 60)
+#define ONE_DAY (24 * 60 * 60)
+
 struct SystemTime;
 unsigned int asSeconds(const SystemTime& time);
 SystemTime asSystemTime(const RtcDateTime& time);
+unsigned int cutToOneDay(const unsigned int& seconds);
+unsigned short cutToOneHour(const unsigned int& seconds);
 
 struct SystemTime {
     unsigned short hour;
@@ -16,6 +22,14 @@ struct SystemTime {
 
     friend bool operator == (const SystemTime& left, const RtcDateTime& right) {
         return asSeconds(left) == asSeconds(asSystemTime(right));
+    }
+
+	friend bool operator != (const SystemTime& left, const SystemTime& right) {
+        return asSeconds(left) != asSeconds(right);
+    }
+
+    friend bool operator != (const SystemTime& left, const RtcDateTime& right) {
+        return asSeconds(left) != asSeconds(asSystemTime(right));
     }
 
     friend bool operator < (const SystemTime& left, const RtcDateTime& right) {
@@ -42,10 +56,20 @@ struct SystemTime {
 		if (left > right) {
 			return asSystemTime(asSeconds(left) - asSeconds(right));
 		} else {
-			//			12		   +		24		 *		   13		 /		 24        + 1   -       13 = 12 + 24 * (0 + 1) - 13 = 36 - 13 = 23
-			//			12		   +		24		 *		   25		 /		 24		   + 1	 - 		 25 = 12 + 24 * (1 + 1) - 25 = (60 - 25) % 24 = 35
-			return asSystemTime((asSeconds(left) + ((24 * 60 * 60) * ((asSeconds(right) / (24 * 60 * 60) + 1)) - asSeconds(right))) % 24);
+			return asSystemTime(
+				cutToOneDay(
+					asSeconds(left) + (ONE_DAY * ((asSeconds(right) / ONE_DAY + 1)) - asSeconds(right))
+				)
+			);
 		}
+	}
+
+	friend void operator += (SystemTime& left, const SystemTime& right) {
+		left = left + right;
+	}
+
+	friend void operator -= (SystemTime& left, const SystemTime& right) {
+		left = left - right;
 	}
 };
 
@@ -62,24 +86,69 @@ unsigned int asSeconds(const SystemTime& time) {
 }
 
 SystemTime asSystemTime(const unsigned int& seconds) {
-	const unsigned int resultSeconds = seconds % (24 * 60 * 60);
+	const unsigned int resultSeconds = cutToOneDay(seconds);
 	
 	SystemTime result {
-		.hour = resultSeconds / (60 * 60),
-		.minute = (resultSeconds % (60 * 60)) / 60
+		.hour = resultSeconds / ONE_HOUR,
+		.minute = cutToOneHour(resultSeconds) / ONE_MINUTE
 	};
 
 	return result;
 }
 
-SystemTime distanceBetween(const SystemTime& t1, const SystemTime& t2) {
-	return asSystemTime((24 * 60 * 60 - asSeconds(t1) + asSeconds(t2)) % 24 * 60 * 60);
+unsigned int distanceBetween(const unsigned int& t1Seconds, const unsigned int& t2Seconds) {
+	return cutToOneDay(ONE_DAY - t1Seconds + t2Seconds);
 }
 
-SystemTime shorterDistanceBetween(const SystemTime& t1, const SystemTime& t2) {
-	return t1 > t2 ? distanceBetween(t2, t1) : distanceBetween(t1, t2);
+unsigned int distanceBetween(const SystemTime& t1, const SystemTime& t2) {
+	return distanceBetween(asSeconds(t1), asSeconds(t2));
 }
 
-SystemTime longerDistanceBetween(const SystemTime& t1, const SystemTime& t2) {
-	return t1 > t2 ? distanceBetween(t1, t2) : distanceBetween(t2, t1);
+unsigned int distanceBetween(const SystemTime& t1, const unsigned int& t2Seconds) {
+	return distanceBetween(asSeconds(t1), t2Seconds);
+}
+
+unsigned int distanceBetween(const unsigned int& t1Seconds, const SystemTime& t2) {
+	return distanceBetween(t1Seconds, asSeconds(t2));
+}
+
+unsigned int absoluteDistanceBetween(const unsigned int& t1Seconds, const unsigned int& t2Seconds) {
+	const unsigned int
+		firstDistance = distanceBetween(t1Seconds, t2Seconds),
+		secondDistance = distanceBetween(t2Seconds, t1Seconds);
+
+	return firstDistance > secondDistance
+		? secondDistance
+		: firstDistance;
+}
+
+unsigned int absoluteDistanceBetween(const SystemTime& t1, const SystemTime& t2) {
+	return absoluteDistanceBetween(asSeconds(t1), asSeconds(t2));
+}
+
+unsigned int absoluteDistanceBetween(const SystemTime& t1, const unsigned int& t2Seconds) {
+	return absoluteDistanceBetween(asSeconds(t1), t2Seconds);
+}
+
+unsigned int absoluteDistanceBetween(const unsigned int& t1Seconds, const SystemTime& t2) {
+	return absoluteDistanceBetween(t1Seconds, asSeconds(t2));
+}
+
+
+unsigned int cutToOneDay(const unsigned int& seconds) {
+	if (seconds < ONE_DAY) {
+		return seconds;
+	}
+
+	const unsigned short numberOfDaysIncludedInSelectedTime = seconds / ONE_DAY;
+	return seconds - ONE_DAY * numberOfDaysIncludedInSelectedTime;
+}
+
+unsigned short cutToOneHour(const unsigned int& seconds) {
+	if (seconds < ONE_HOUR) {
+		return seconds;
+	}
+
+	const unsigned short numberOfHoursIncludedInSelectedTime = seconds / ONE_HOUR;
+	return seconds - ONE_HOUR * numberOfHoursIncludedInSelectedTime;
 }
